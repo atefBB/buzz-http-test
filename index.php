3f631afe92d8ce1b838629cba3a637320e234734
Atef Ben Ali <atef.bettaib@gmail.com>
2018-08-03 15:47:22 +0100

    perf: get response contents as HTML

--- index.php
+++ index.php
@@ -5,4 +5,4 @@ include_once './vendor/autoload.php';
 $browser  = new Buzz\Browser;
 $response = $browser->get('http://www.google.com');
 
-var_dump($response->getBody());die();
+var_dump($response->getBody()->getContents());die();
